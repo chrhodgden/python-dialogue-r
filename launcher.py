419ddade1201340df9e41f7aab8e749665f90738
TinWoodman92 <chrhodgden@gmail.com>
2023-04-05 16:54:32 -0500

    Removed comments. Procedure uses source_file.r

--- launcher.py
+++ launcher.py
@@ -1,25 +1,16 @@
-import idlelib
 import os
 from subprocess import *
 
-# If I can't find the R path, I could have a dedicated r procedure that finds R.home() and puts that into a text file.
-
 print(os.getcwd())
 
-#open demp.py
-# demo_fil = os.path.join(os.getcwd(),'demo.py')
-# if os.path.exists(demo_fil):
-# 	idle_bat = idlelib.__file__.replace('__init__.py','idle.bat')
-# 	Popen(f'{idle_bat} "{demo_fil}"')
-
-tar_fil = os.path.join(os.getcwd(),'demo.r')
+tar_fil = os.path.join(os.getcwd(),'source_file.r')
 print(tar_fil)
 if os.path.exists(tar_fil):
-	rscript_exe = 'C:/Program Files/R/R-4.2.2/bin/Rscript.exe'
-	rscript_exe = 'Rscript'
-	print(os.path.exists(rscript_exe))
+	rscript_path = 'C:/Program Files/R/R-4.2.2/bin/Rscript.exe'
+	print(os.path.exists(rscript_path))
+	rscript_cmd = 'Rscript'
 	run(
-		f'{rscript_exe} "{tar_fil}"',
+		f'{rscript_cmd} "{tar_fil}"',
 		cwd = os.getcwd(),
 		start_new_session = True
 		)

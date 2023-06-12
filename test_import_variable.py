3527e1a22e559f8a3d6e87bb2d24adb6ae2c64a4
TinWoodman92 <chrhodgden@gmail.com>
2023-06-11 21:04:42 -0500

    Migrated use-case launcher.py to test-case test_import_variable.py. R file renamed accordingly.

---
+++ test_import_variable.py
@@ -0,0 +1,30 @@
+import unittest
+import dialoguer
+
+
+class TestEmployee(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		pass
+
+	@classmethod
+	def tearDownClass(cls):
+		pass
+
+	def setUp(self):
+		self.src_fil_r = dialoguer.Dialogue('test_import_variable.r')
+		self.src_fil_r.open()
+	
+	def tearDown(self):
+		self.src_fil_r.close()
+
+	def test_import_variable(self):
+		msg_1 = self.src_fil_r.import_variable('msg_1')
+		msg_2 = self.src_fil_r.import_variable('msg_2')
+		self.assertEqual(msg_1, b'Initializing Client - R')
+		self.assertEqual(msg_2, b'Initialized Client - R')
+
+
+if __name__ == '__main__':
+	unittest.main()

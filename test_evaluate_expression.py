2b1034a219ec07b4b0811f070e37f01853d6e262
TinWoodman92 <chrhodgden@gmail.com>
2023-06-25 20:03:51 -0500

    initial design of evaluate_expression test

---
+++ test_evaluate_expression.py
@@ -0,0 +1,27 @@
+import unittest
+import dialoguer
+
+
+class TestImportVariable(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		cls.src_fil_r = dialoguer.Dialogue('test_evaluate_expression.r')
+
+	@classmethod
+	def tearDownClass(cls):
+		cls.src_fil_r.close()
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_evaluate_expression(self):
+		res_1 = self.src_fil_r.evaluate_expression('sum', 1, 2)
+		self.assertEqual(res_1, 3)
+
+
+if __name__ == '__main__':
+	unittest.main()

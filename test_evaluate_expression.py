13808c38f81d0149b37b545180b2851c8903b4c6
TinWoodman92 <chrhodgden@gmail.com>
2023-06-27 21:18:00 -0500

    added kwargs test

--- test_evaluate_expression.py
+++ test_evaluate_expression.py
@@ -6,17 +6,17 @@ class TestEvaluateExpression(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(cls):
-		pass
+		cls.src_fil_r = dialoguer.Dialogue('test_evaluate_expression.r')
 
 	@classmethod
 	def tearDownClass(cls):
-		pass
+		cls.src_fil_r.close()
 
 	def setUp(self):
-		self.src_fil_r = dialoguer.Dialogue('test_evaluate_expression.r')
+		pass
 
 	def tearDown(self):
-		self.src_fil_r.close()
+		pass
 
 	def test_built_in_method(self):
 		res_1 = self.src_fil_r.evaluate_expression('sum', 1, 2)
@@ -25,6 +25,14 @@ class TestEvaluateExpression(unittest.TestCase):
 	def test_defined_method(self):
 		res_1 = self.src_fil_r.evaluate_expression('test_method', 1, 2)
 		self.assertEqual(res_1, 3)
+
+	def test_kwargs(self):
+		kwargs = {
+			'arg_1': 2,
+			'arg_2': 3
+		}
+		res_1 = self.src_fil_r.evaluate_expression('test_kwargs', **kwargs)
+		self.assertEqual(res_1, 6)
 
 
 if __name__ == '__main__':
--- test_evaluate_expression.r
+++ test_evaluate_expression.r
@@ -1,4 +1,9 @@
-test_method <- function (arg_1, arg_2) {
+test_method <- function(arg_1, arg_2) {
 	result <- arg_1 + arg_2
+	return(result)
+}
+
+test_kwargs <- function(arg_1 = 1, arg_2 = 1) {
+	result <- arg_1 * arg_2
 	return(result)
 }

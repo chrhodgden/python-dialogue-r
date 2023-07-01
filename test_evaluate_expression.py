6befebe3030105bd88f22970a369178a2b7212e1
TinWoodman92 <chrhodgden@gmail.com>
2023-07-01 16:33:14 -0500

    added args and kwargs test.

--- test_evaluate_expression.py
+++ test_evaluate_expression.py
@@ -4,19 +4,19 @@ import dialoguer
 
 class TestEvaluateExpression(unittest.TestCase):
 
-	@classmethod
-	def setUpClass(cls):
-		cls.src_fil_r = dialoguer.Dialogue('test_evaluate_expression.r')
+	# @classmethod
+	# def setUpClass(cls):
+	# 	cls.src_fil_r = dialoguer.Dialogue('test_evaluate_expression.r')
 
-	@classmethod
-	def tearDownClass(cls):
-		cls.src_fil_r.close()
+	# @classmethod
+	# def tearDownClass(cls):
+	# 	cls.src_fil_r.close()
 
 	def setUp(self):
-		pass
+		self.src_fil_r = dialoguer.Dialogue('test_evaluate_expression.r')
 
 	def tearDown(self):
-		pass
+		self.src_fil_r.close()
 
 	def test_built_in_method(self):
 		res_1 = self.src_fil_r.evaluate_expression('sum', 1, 2)
@@ -33,6 +33,16 @@ class TestEvaluateExpression(unittest.TestCase):
 		}
 		res_1 = self.src_fil_r.evaluate_expression('test_kwargs', **kwargs)
 		self.assertEqual(res_1, 6)
+
+	def test_args_and_kwargs(self):
+		args = [2, 3]
+		kwargs = {
+			'kwarg_1': 2,
+			'kwarg_2': 3
+		}
+		res_1 = self.src_fil_r.evaluate_expression('test_args_and_kwargs', *args, **kwargs)
+		res_ctrl = args[0] * args[1] * kwargs['kwarg_1'] * kwargs['kwarg_2']
+		self.assertEqual(res_ctrl, res_1)
 
 
 if __name__ == '__main__':

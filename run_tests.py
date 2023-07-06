1398f37b1ffd35dd42e2e76695429bea2c454d18
TinWoodman92 <chrhodgden@gmail.com>
2023-07-06 18:48:09 -0500

    staged specific testing.

--- run_tests.py
+++ run_tests.py
@@ -3,5 +3,7 @@ import unittest
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite = loader.discover("tests", pattern="test_*.py")
+    # suite = loader.loadTestsFromName("tests.test_assign_variable.TestAssignVariable.test_assign_double")
+    # suite = loader.loadTestsFromName("tests.test_assign_variable.TestAssignVariable")
     runner = unittest.TextTestRunner()
     result = runner.run(suite)

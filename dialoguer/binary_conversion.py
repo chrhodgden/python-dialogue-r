c7e7609ca56c81c6517b8d685cf2ce41dd9aae3a
TinWoodman92 <chrhodgden@gmail.com>
2023-07-06 18:47:12 -0500

    added convert from float and boolean.

--- dialoguer/binary_conversion.py
+++ dialoguer/binary_conversion.py
@@ -7,8 +7,10 @@ def bin_conv(data, data_type = None):
 		conv_data = bytes(data, 'utf-8')
 	elif type(data) == int and data_type == None:
 		conv_data = data.to_bytes(32, 'little')
+	elif type(data) == float and data_type == None:
+		conv_data = struct.pack('d', data)
 	elif type(data) == bool and data_type == None:
-		conv_data = bytes(data)
+		conv_data = int(data).to_bytes(4, 'little')
 	
 	#convert from binary 
 	elif type(data) == bytes and data_type == str:
@@ -17,7 +19,7 @@ def bin_conv(data, data_type = None):
 	elif type(data) == bytes and data_type == int:
 		conv_data = int.from_bytes(data, 'little')
 	elif type(data) == bytes and data_type == bool:
-		conv_data = bool(data[0])
+		conv_data = bool(int.from_bytes(data, 'little'))
 	elif type(data) == bytes and data_type == float:
 		conv_data = struct.unpack('d', data)[0]
 

c996a289b41c750ec8016a74a9a8b3532484010b
TinWoodman92 <chrhodgden@gmail.com>
2023-07-06 18:44:37 -0500

    Fixed conversion tests to pass.

--- tests/test_binary_conversion.py
+++ tests/test_binary_conversion.py
@@ -1,9 +1,6 @@
 import unittest
 from dialoguer.binary_conversion import bin_conv
 
-# some of these test currently fail because bytes are reversed. 
-# the binary conversions work with R's tendancy to reverse the sockets i/o
-# perhaps we can put reversals in this test
 class TestBinaryConversion(unittest.TestCase):
 
 	@classmethod
@@ -20,21 +17,19 @@ class TestBinaryConversion(unittest.TestCase):
 	def tearDown(self):
 		pass
 
-	def test_convert_to_binary(self):
-		bin_1 = b'\x00\x00\x00\x00\x00\x00\x01\x00'
-		bin_2 = bin_conv(2)
-		self.assertEqual(bin_1, bin_2)
-
-	def test_convert_from_binary(self):
-		bin_1 = b'\x00\x01\x00\x00\x00\x00\x00\x00'
-		var_1 = 2
-		var_2 = bin_conv(bin_1, type(var_1))
-		self.assertEqual(var_1, var_2)
+	def test_convert_boolean(self):
+		data_0 = False
+		data_1 = True
+		conv_data_0 = bin_conv(data_0)
+		conv_data_1 = bin_conv(data_1)
+		conv_data_0 = bin_conv(conv_data_0, type(data_0))
+		conv_data_1 = bin_conv(conv_data_1, type(data_1))
+		self.assertEqual(data_0, conv_data_0)
+		self.assertEqual(data_1, conv_data_1)
 
 	def test_integer_conversion(self):
 		for i in range(1000):
 			bin_i = bin_conv(i)
-			bin_i = bytes(reversed(bin_i))
 			var_i = bin_conv(bin_i, type(i))
 			self.assertEqual(i, var_i)
 
@@ -44,14 +39,19 @@ class TestBinaryConversion(unittest.TestCase):
 		for i in range(1000):
 			str_i = str(i)
 			bin_i = bin_conv(str_i)
-			print(bin_i, type(bin_i))
 			var_i = bin_conv(bin_i, type(str_i))
 			self.assertEqual(str_i, var_i)
 		for s in str_base:
 			bin_s = bin_conv(s)
-			bin_s = bytes(reversed(bin_s))
 			var_s = bin_conv(bin_s, type(s))
 			self.assertEqual(s, var_s)
+
+	def test_float_conversion(self):
+		for i in range(0, 1000):
+			flt_i = i + 0.5
+			bin_i = bin_conv(flt_i)
+			var_i = bin_conv(bin_i, type(flt_i))
+			self.assertEqual(flt_i, var_i)
 
 
 if __name__ == '__main__':

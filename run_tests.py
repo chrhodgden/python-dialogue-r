5753265e7ba136fdba10ca8ea384837a18179de6
TinWoodman92 <chrhodgden@gmail.com>
2023-07-07 17:29:43 -0500

    cleaned up.

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,8 @@
 import unittest
 
 if __name__ == "__main__":
-    loader = unittest.TestLoader()
-    suite = loader.discover("tests", pattern="test_*.py")
-    # suite = loader.loadTestsFromName("tests.test_reliability.TestReliability.test_no_return")
-    # suite = loader.loadTestsFromName("tests.test_assign_variable.TestAssignVariable")
-    runner = unittest.TextTestRunner()
-    result = runner.run(suite)
+	loader = unittest.TestLoader()
+	suite = loader.discover("tests", pattern="test_*.py")
+	# suite = loader.loadTestsFromName("tests.test_reliability.TestReliability.test_no_return")
+	runner = unittest.TextTestRunner()
+	result = runner.run(suite)

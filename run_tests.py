e7ce2f0a81ec5992d0fe32086cc5e217fe91a2ad
TinWoodman92 <chrhodgden@gmail.com>
2023-07-10 17:12:19 -0500

    update

--- run_tests.py
+++ run_tests.py
@@ -2,7 +2,8 @@ import unittest
 
 if __name__ == "__main__":
 	loader = unittest.TestLoader()
-	suite = loader.discover("tests", pattern="test_*.py")
-	# suite = loader.loadTestsFromName("tests.test_reliability.TestReliability.test_no_return")
+	# suite = loader.discover("tests", pattern="test_*.py")
+	suite = loader.loadTestsFromName("tests.test_save_environment.TestSaveEnvironment.test_save_environment")
+	# suite = loader.loadTestsFromName("tests.test_assign_variable.TestAssignVariable.test_assign_integer")
 	runner = unittest.TextTestRunner()
 	result = runner.run(suite)
